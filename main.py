766dd0b7c7cb0a874a035fa8de7023c97b1e3cb0
jeichel09 <dunyto@gmail.com>
2020-08-04 12:38:04 +0200

    eine erste Version

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from models import db, User
+
+app = Flask(__name__)
+db.create_all()
+
+@app.route("/", methods=["GET"])
+def index():
+    all_users = db.query(User).all()
+    return render_template("index.html", users=all_users)
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    user_id = request.args.get("id")
+    if user_id:
+        db.query(User).get(user_id).delete()
+        db.commit()
+    return make_response(redirect(url_for('index')))
+
+@app.route("/add_edit", methods=["GET", "POST"])
+def add_edit():
+    user = None
+    user_id = request.args.get("id")
+    if user_id:
+        user = db.query(User).get(user_id)
+    if request.method == "GET":  # show form
+        return render_template("add_edit.html", user=user)
+    else: # handle submission
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        if user: #update
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+        else: # create
+            user = User(first_name=first_name, last_name=last_name, email=email)
+        db.add(user)
+        db.commit()
+        return make_response(redirect(url_for('index')))
+
+if __name__ == "__main__":
+    app.run()
+    
+    
---
+++ models.py
@@ -0,0 +1,10 @@
+import os
+from sqla_wrapper import SQLAlchemy
+
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String)
